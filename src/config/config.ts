7a3f6a38eaa2102bbc3944c33dbab85d2a3c2ce9
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-05-05 00:22:23 -0400

    Changement important lol

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const SERVER_HOST_NAME = process.env.SERVER_HOST_NAME || "localhost";
-const SERVER_PORT = process.env.PORT || "3000";
+const SERVER_PORT = process.env.PORT || process.env.SERVER_PORT || "5000";
 
 export default {
     server: {
