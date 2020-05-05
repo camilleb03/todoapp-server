3fda82c96cc8e4ef2c4d13eebfa9c78cbc4eda45
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-05-04 22:55:07 -0400

    Connexion a Heroku Postgres

--- src/connexion.ts
+++ src/connexion.ts
@@ -1,11 +1,8 @@
 import { Pool } from 'pg'
 
 const pool = new Pool({
-    user: 'Camille',
-    host:'localhost',
-    password:'',
-    database:'todoapi',
-    port:5432
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
 
 export default pool;
